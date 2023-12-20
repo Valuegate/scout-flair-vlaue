3b58a6905c1524d3339d85a7713cc6b1fede7c69
Lizzy223 <koladeelizabeth01@gmail.com>
2023-12-20 16:08:03 +0100

    update:dashboard start

--- src/dashboard/pages/Settings.jsx
+++ src/dashboard/pages/Settings.jsx
@@ -1,4 +1,4 @@
-import { Box, Checkbox, Text } from '@chakra-ui/react'
+import { Box, Checkbox, Text, Button } from '@chakra-ui/react'
 import React from 'react'
 import InputValue from '../components/InputValue'
 
@@ -6,9 +6,9 @@ const Settings = () => {
   return (
     <Box w='full' display='flex' flexDir={['column','row']} p='1rem' justifyContent='space-between' >
 
-        <Box w={['full','40%']}  p='1rem' mt='1rem' >
+        <Box w={['full','40%']}  p='1rem 2rem' mt='1rem' >
             <Text fontWeight='700'>Edit Profile</Text>
-            <Box  borderRadius='8px' p='.5rem' bg='#fff'>
+            <Box  borderRadius='8px' mt='.5rem' p='1rem' bg='#fff'>
                 <InputValue placeholder='Name'  text='First Name'/>
                 <InputValue placeholder='Last name'  text='Last Name'/>
                 <InputValue placeholder='Nigeria'   text='Location'/>
@@ -20,15 +20,15 @@ const Settings = () => {
                 <InputValue placeholder='Name'  text='Address'/>
             </Box>
         </Box>
-        <Box w={['full','40%']}  p='1rem' mt='1rem' >
+        <Box w={['full','40%']}  p='1rem 2rem' mt='1rem' >
             <Text fontWeight='700'>Change Password</Text>
-            <Box  borderRadius='8px' p='.5rem' bg='#fff'>
+            <Box  borderRadius='8px' mt='.5rem' p='.5rem' bg='#fff'>
                 <InputValue placeholder='********' type='password' text='Current Password'/>
                 <InputValue placeholder='********' type='password'  text='New Password'/>
                 <InputValue placeholder='********'  text='Confirm Password'/>
             </Box>
              <Text mt='1rem' fontWeight='700'>Change Settings</Text>
-            <Box  borderRadius='8px' p='1rem' bg='#fff'>
+            <Box  borderRadius='8px' mt='.5rem' p='1rem ' bg='#fff'>
                 <Box display='flex' justifyContent='space-between' w='full' boxShadow='3px 3px 3px #ccc' p='1rem' borderRadius='4px' >
                     <Box >
                         <Text fontWeight='700' >Match Notification</Text>
@@ -58,9 +58,12 @@ const Settings = () => {
                     <Checkbox />
                 </Box>
             </Box>
+            <Box w='full' display='flex' mt='1rem' justifyContent='flex-end'>
+                <Button bg='#E5AA42' color='#fff' p='.75rem' cursor='pointer' >Save</Button>
+            </Box>
         </Box>
 
-
+            
     </Box>
   )
 }
